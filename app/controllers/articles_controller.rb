5d9899ac093d151c03718503a9a1c30100356a16
kazuki0925 <kazukinyo0925@gmail.com>
2020-12-17 22:56:06 +0900

    記事編集機能の実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_article, only: [:show, :edit, :update, :destroy] 
 
   def index
     @articles = Article.includes(:user).order("created_at DESC")
@@ -19,21 +20,31 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def edit
-
   end
 
   def update
+    if @article.update(article_params)
+      redirect_to article_path(@article) 
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @article.destroy
+    redirect_to root_path
   end
 
   private
   def article_params
     params.require(:article).permit(:title, :text, :image).merge(user_id: current_user.id)
   end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
 end
