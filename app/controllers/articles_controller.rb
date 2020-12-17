8310a75860ed2f8565b13336e31df81ffb364e71
kazuki0925 <kazukinyo0925@gmail.com>
2020-12-17 16:38:09 +0900

    投稿記事の表示

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @articles = Article.all
+    @articles = Article.includes(:user).order("created_at DESC")
   end
 
   def new
