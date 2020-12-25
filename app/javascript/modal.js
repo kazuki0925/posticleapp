ae94300b3cf9a16363ca8ebafab8d9b5e6aca27a
kazuki0925 <kazukinyo0925@gmail.com>
2020-12-25 18:32:32 +0900

    userモデルの単体テストコード

--- app/javascript/modal.js
+++ app/javascript/modal.js
@@ -1,14 +1,12 @@
-function modal() {
-  const pullDownButton = document.getElementById("img-menu-icon")
-  const modalMenu = document.getElementById("modal-menu")
-
-  pullDownButton.addEventListener('click', function(){
-    if (modalMenu.getAttribute("class") == "hidden") {
-      modalMenu.removeAttribute("class", "hidden")
-    } else {
-      modalMenu.setAttribute("class", "hidden")
-    }
-  })
-}
-
-window.addEventListener('load', modal)
+$(function(){
+const menu = document.getElementById('img-menu-icon');
+$('#img-menu-icon').on('click', function(){
+  if (menu.getAttribute("class") != "open-menu") {
+    $('#modal-menu').fadeIn();
+    $('#img-menu-icon').addClass("open-menu");
+  } else {
+    $('#modal-menu').fadeOut();
+    $('#img-menu-icon').removeClass("open-menu");
+  }
+});
+});
